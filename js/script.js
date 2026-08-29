eeca0fbfb09403a2c3795e008511b1267ad24b54
Syed-Faizeen <syedfaizeen2005@gmail.com>
2026-08-29 11:23:55 +0600

    made the button apply dark mode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const outer = document.getElementById("btn-outer")
 const toggleBtn = document.getElementById("toggle-btn")
+const body = document.getElementById("bg")
 
 function toggle() {
 	outer.classList.toggle("on")
 	toggleBtn.classList.toggle("on")
+	body.classList.toggle("on")
 }
