90fe9ddff6ef38ff98ea70e1a3e67947e2d376ee
Syed-Faizeen <syedfaizeen2005@gmail.com>
2026-08-29 10:52:19 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const outer = document.getElementById("btn-outer")
+const toggleBtn = document.getElementById("toggle-btn")
+
+function toggle() {
+	outer.classList.toggle("on")
+	toggleBtn.classList.toggle("on")
+}
